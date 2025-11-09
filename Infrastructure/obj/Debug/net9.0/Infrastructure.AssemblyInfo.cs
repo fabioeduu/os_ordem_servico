d9597b679a389401af5c5127187cce7e215c148c
fabioeduu <133786674+fabioeduu@users.noreply.github.com>
2025-11-09 16:14:01 -0300

    projeto atualizado

--- Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbf64ecddf03b167d745541c974669d6a6753b38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7343d1ac64f013eef0bcfdd632a02a9faec1237")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
